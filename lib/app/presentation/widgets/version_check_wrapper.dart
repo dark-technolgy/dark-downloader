7fe6ac03aee682c11fb617d323925cbd9fa27d8d
keenxcom <keenxcom@github.com>
2026-06-25 07:33:09 +0300

    Fix missing imports in version_check_wrapper and update Android compileSdk to 36 for plugin compatibility. Version v1.1.7

--- lib/app/presentation/widgets/version_check_wrapper.dart
+++ lib/app/presentation/widgets/version_check_wrapper.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../providers/remote_config_provider.dart';
+import '../../services/update_service.dart';
 
 class VersionCheckWrapper extends StatefulWidget {
   final RemoteConfigState config;
